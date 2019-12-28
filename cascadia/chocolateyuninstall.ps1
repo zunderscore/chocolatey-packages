a9e18fe15bdc8c1dce6088157caa4e3ef8df1ea3
Zack Williamson <zwilliamson@outlook.com>
2019-12-27 22:25:16 -0500

    More graceful upgrade handling and slightly better logging

--- cascadia/chocolateyuninstall.ps1
+++ cascadia/chocolateyuninstall.ps1
@@ -4,6 +4,10 @@ $packageArgs = @{
   packageName   = '{{packageName}}'
 }
 
+Write-Output "Uninstalling {{displayName}} font..."
+
 if ((Uninstall-ChocolateyFont "{{filename}}") -eq 1) {
     Write-Error "Error uninstalling {{displayName}} font"
+} else {
+    Write-Output "{{displayName}} font uninstalled successfully"
 }
